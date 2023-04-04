4f83117245085d481961d25b7207d4e360a67af7
Sam <sam.sutcliffe2@gmail.com>
2023-04-04 17:03:58 +0100

    Updated beam

--- beam_analysis.py
+++ beam_analysis.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-df_noinc = pd.read_csv("output_{}/final.csv".format("true"))
-for dname in ["true","inc","jaumann","truesdale","logspin"]:
-    df = pd.read_csv("output_{}/final.csv".format(dname))
+df_noinc = pd.read_csv("output_rate_{}/final.csv".format("true"))
+for dname in ["true","inc","logspin"]:
+    df = pd.read_csv("output_rate_{}/final.csv".format(dname))
     plt.scatter(df["coord_x"],df["coord_y"],label=dname)
     dx = (df["coord_x"] - df_noinc["coord_x"]).pow(2)
     dy = (df["coord_y"] - df_noinc["coord_y"]).pow(2)
