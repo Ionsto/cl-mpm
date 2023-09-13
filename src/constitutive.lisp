738f212bc57fb2d5abc60b4199dd52f847c9baa2
Sam <sam.sutcliffe2@gmail.com>
2023-09-13 02:04:09 +0100

    Updated strain update

--- src/constitutive.lisp
+++ src/constitutive.lisp
@@ -12,7 +12,7 @@
     )
   )
 (in-package :cl-mpm/constitutive)
-(declaim (optimize (debug 3) (safety 3) (speed 3)))
+(declaim (optimize (debug 0) (safety 0) (speed 3)))
 
 (defun linear-elastic-matrix (E nu)
   "Create an isotropic linear elastic matrix"
