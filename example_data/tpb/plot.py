7508248cd277bb33f81cc68f6674cc08478a23df
Sam <sam.sutcliffe2@gmail.com>
2023-11-13 08:27:53 +0000

    Updated convergance of three point bending

--- example_data/tpb/plot.py
+++ example_data/tpb/plot.py
@@ -4,9 +4,9 @@ import pandas as pd
 data = pd.read_csv("load-disp.csv")
 plt.plot(data["disp"],data["load"],label="Data")
 
-for i in [1,2]:
-    mpm = pd.read_csv("conv/disp.csv")
+for i in ["0.5","1","2"]:
+    mpm = pd.read_csv("conv/disp-{}.csv".format(i))
     #plt.plot(-1e3*mpm["disp"],2*13e-3*mpm["load"],label="MPM")
-    plt.plot(-1e3*mpm["disp"],2*0.5*13e-3*mpm["nload"],label="Reaction - {}".format(i))
+    plt.plot(-1e3*mpm["disp"],2*0.5*13e-3*mpm["load"],label="Reaction - {}".format(i))
 plt.legend()
 plt.show()
