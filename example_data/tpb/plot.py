a27ee7b1e8e3adcf7c7cbcbbf91f10c22d414a86
Sam <sam.sutcliffe2@gmail.com>
2023-11-15 08:58:47 +0000

    Updated plotting

--- example_data/tpb/plot.py
+++ example_data/tpb/plot.py
@@ -4,9 +4,14 @@ import pandas as pd
 data = pd.read_csv("load-disp.csv")
 plt.plot(data["disp"],data["load"],label="Data")
 
+scales = [1,1,1]
 for i in ["0.5","1","2"]:
     mpm = pd.read_csv("conv/disp-{}.csv".format(i))
-    #plt.plot(-1e3*mpm["disp"],2*13e-3*mpm["load"],label="MPM")
-    plt.plot(-1e3*mpm["disp"],2*0.5*13e-3*mpm["load"],label="Reaction - {}".format(i))
+    plt.plot(-1e3*mpm["disp"],13e-3*mpm["load"],label="Reaction - h/{}".format(i))
+for i in ["4"]:
+    mpm = pd.read_csv("conv/disp-{}.csv".format(i))
+    plt.plot(-1e3*mpm["disp"],13e-3*mpm["nload"],label="Reaction - h/{}".format(i))
+plt.xlabel("Displacement (mm)")
+plt.ylabel("Load (N)")
 plt.legend()
 plt.show()
