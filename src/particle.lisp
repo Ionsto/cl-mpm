18adcf1471c1fc29566569f45b7069b938068bd5
Sam <sam.sutcliffe2@gmail.com>
2024-03-06 16:23:13 +0000

    added plasticity

--- src/particle.lisp
+++ src/particle.lisp
@@ -1899,7 +1899,7 @@
 ;; (defclass particle-elastic-damage-ideal (particle-concrete)
 ;;   )
 
-(defclass particle-limestone (particle-concrete)
+(defclass particle-limestone (particle-concrete particle-mc)
   (
    (compression-ratio
     :accessor mp-compression-ratio
@@ -1910,6 +1910,11 @@
     :type DOUBLE-FLOAT
     :initarg :internal-length
     :initform 1d0)
+   (enable-plasticity
+    :accessor mp-enable-plasticity
+    :initarg :enable-plasticity
+    :initform t
+    )
    )
   (:documentation "A concrete damage model"))
 
@@ -1941,15 +1946,51 @@
                (calc-pressure pressure-func)
                )
       mp
-    (declare (double-float pressure damage)
-             (function calc-pressure))
-    ;; Non-objective stress intergration
-    (setf stress-undamaged (cl-mpm/constitutive::linear-elastic-mat strain de))
-
-    (setf stress (magicl:scale stress-undamaged 1d0))
-    (when (> damage 0.0d0)
-      (let ((degredation (max 1d-9 (expt (- 1d0 damage) 1d0))))
-        (magicl:scale! stress degredation)))
+    (with-accessors
+          ((phi mp-phi)
+           (psi mp-psi)
+           (E mp-E)
+           (nu mp-nu)
+           (coheasion mp-c)
+           (plastic-strain mp-strain-plastic)
+           (yield-func mp-yield-func)
+           (enable-plasticity mp-enable-plasticity)
+           (ps-vm mp-strain-plastic-vm)
+           )
+        mp
+      (declare (double-float pressure damage)
+               (function calc-pressure))
+      ;; Non-objective stress intergration
+      (setf stress-undamaged (cl-mpm/constitutive::linear-elastic-mat strain de))
+      (if enable-plasticity
+          (progn
+            (multiple-value-bind (sig eps-e f)
+                (cl-mpm/constitutive::mc-plastic stress-undamaged
+                                                 de
+                                                 strain
+                                                 E
+                                                 nu
+                                                 phi
+                                                 psi
+                                                 coheasion)
+              (setf stress
+                    sig
+                    plastic-strain (magicl:.- strain eps-e)
+                    yield-func f)
+              (setf strain eps-e))
+            (incf ps-vm
+                  (multiple-value-bind (l v)
+                      (cl-mpm/utils:eig (cl-mpm/utils:voigt-to-matrix (cl-mpm/particle::mp-strain-plastic mp)))
+                    (destructuring-bind (s1 s2 s3) l
+                      (sqrt
+                       (/ (+ (expt (- s1 s2) 2d0)
+                             (expt (- s2 s3) 2d0)
+                             (expt (- s3 s1) 2d0)
+                             ) 2d0))))))
+          (setf stress (magicl:scale stress-undamaged 1d0)))
+      (when (> damage 0.0d0)
+        (let ((degredation (max 1d-9 (expt (- 1d0 damage) 1d0))))
+          (magicl:scale! stress degredation))))
     stress
     ))
 
