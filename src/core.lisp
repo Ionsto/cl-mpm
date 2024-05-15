4ea56858107542df360aa7fea17c4b7c6c55d3cf
Sam <sam.sutcliffe2@gmail.com>
2024-05-15 09:38:31 +0100

    Updated damping

--- src/core.lisp
+++ src/core.lisp
@@ -1450,7 +1450,7 @@ weight greater than 0, calling func with the mesh, mp, node, svp, and grad"
           (magicl:scale! acc 0d0)
           ;;Set acc to f/m
           (cl-mpm/fastmath:fast-fmacc acc force (/ 1d0 (* mass mass-scale)))
-          (cl-mpm/fastmath:fast-fmacc acc vel (/ (* damping -1d0) mass-scale))
+          (cl-mpm/fastmath:fast-fmacc acc vel (/ (* damping -1d0) (* mass mass-scale)))
           (cl-mpm/fastmath:fast-fmacc vel acc dt)
           )))
   (values))
