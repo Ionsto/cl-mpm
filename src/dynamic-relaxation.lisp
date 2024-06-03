e9224b86b040be6f73f7168ea03e5ab2d5c31df0
Sam <sam.sutcliffe2@gmail.com>
2024-06-03 02:30:51 +0100

    Update dr

--- src/dynamic-relaxation.lisp
+++ src/dynamic-relaxation.lisp
@@ -114,14 +114,14 @@
                            nmax
                            (*
                             ;; (cl-mpm/mesh:node-mass node)
-                            ;; (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
+                            (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
                             (cl-mpm/fastmath::mag-squared
                              (magicl:.+ f-ext f-int))))
                      dmax (+
                            dmax
                            (*
                             ;; (cl-mpm/mesh:node-mass node)
-                            ;; (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
+                            (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
                             (cl-mpm/fastmath::mag-squared f-ext))))))))))
     (when (> dmax 0d0)
       (setf oobf (sqrt (/ nmax dmax))))
@@ -159,13 +159,13 @@
                  (setf nmax (+
                              nmax
                              (*
-                              ;; (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
+                              (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
                               (cl-mpm/fastmath::mag-squared
                                (magicl:.+ f-ext f-int))))
                        dmax (+
                              dmax
                              (*
-                              ;; (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
+                              (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
                               (cl-mpm/fastmath::mag-squared f-ext)))))))))))
     (setf nmax (cl-mpm/mpi::mpi-sum nmax))
     (setf dmax (cl-mpm/mpi::mpi-sum dmax))
