5bc0d41bf9a3f2baad09085e407e95098940400c
samjvsutcliffe <sam.sutcliffe@durham.ac.uk>
2024-06-11 11:03:24 +0100

    Updated dr

--- src/dynamic-relaxation.lisp
+++ src/dynamic-relaxation.lisp
@@ -167,19 +167,19 @@
                  ;;          (cl-mpm/fastmath::mag-squared
                  ;;           (magicl:.+ f-ext f-int))
                  ;;          (cl-mpm/fastmath::mag-squared f-ext)))))
-                 (setf nmax (+
+                 (setf nmax (max
                              nmax
                              (*
                               (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
                               (cl-mpm/fastmath::mag-squared
                                (magicl:.+ f-ext f-int))))
-                       dmax (+
+                       dmax (max
                              dmax
                              (*
                               (/ (cl-mpm/mesh::node-volume node) (cl-mpm/mesh::node-volume-true node))
                               (cl-mpm/fastmath::mag-squared f-ext)))))))))))
-    (setf nmax (cl-mpm/mpi::mpi-sum nmax))
-    (setf dmax (cl-mpm/mpi::mpi-sum dmax))
+    (setf nmax (cl-mpm/mpi::mpi-max nmax))
+    (setf dmax (cl-mpm/mpi::mpi-max dmax))
     (if (> dmax 0d0)
         (setf oobf (sqrt (/ nmax dmax)))
       ;;Odd case where we have no forces?
