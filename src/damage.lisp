16d93383ebb3e29013c6b0b67b7e069f8ffbdba1
samjvsutcliffe <117826225+samjvsutcliffe@users.noreply.github.com>
2025-01-04 13:47:45 +0000

    Updated mpi damage

--- src/damage.lisp
+++ src/damage.lisp
@@ -733,7 +733,7 @@ Calls the function with the mesh mp and node"
      (lambda (mp)
        (when (typep mp 'cl-mpm/particle:particle-damage)
          ;; (find-nodal-local-length mesh mp)
-         (find-intergral-local-length mesh mp)
+         ;; (find-intergral-local-length mesh mp)
          ;; (setf (cl-mpm/particle::mp-true-local-length mp)
          ;;       (length-localisation (cl-mpm/particle::mp-local-length mp)
          ;;                            (cl-mpm/particle::mp-local-length-damaged mp)
