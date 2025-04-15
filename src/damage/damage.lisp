46f4cc75317f83c61d1bb6dde5a8ad5e705e91e8
Sam <sam.sutcliffe2@gmail.com>
2025-04-15 11:00:17 +0100

    Updated ice-buoyancy

--- src/damage/damage.lisp
+++ src/damage/damage.lisp
@@ -243,12 +243,13 @@
         ;;Worst case we want dont want our delocal counter to exceed the max incase we get adjusted
         (setf delocal-counter (min (- delocal-counter 1)
                                    delocal-counter-max)))
+
       (cl-mpm:iterate-over-mps
        mps
        (lambda (mp)
          (when (typep mp 'cl-mpm/particle:particle-damage)
-           (damage-model-calculate-y mp dt)
-           )))
+           (damage-model-calculate-y mp dt))))
+
       (if non-local-damage
           (progn
             (when (sim-enable-length-localisation sim)
@@ -757,12 +758,6 @@ Calls the function with the mesh mp and node"
      mps
      (lambda (mp)
        (when (typep mp 'cl-mpm/particle:particle-damage)
-         ;; (find-nodal-local-length mesh mp)
-         ;; (find-intergral-local-length mesh mp)
-         ;; (setf (cl-mpm/particle::mp-true-local-length mp)
-         ;;       (length-localisation (cl-mpm/particle::mp-local-length mp)
-         ;;                            (cl-mpm/particle::mp-local-length-damaged mp)
-         ;;                            (cl-mpm/particle::mp-damage mp)))
          (with-accessors ((damage-inc cl-mpm/particle::mp-damage-increment)
                           (damage-inc-local cl-mpm/particle::mp-local-damage-increment)
                           (damage cl-mpm/particle::mp-damage)
